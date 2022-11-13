1a9b0ad3ea847b6ffebba27a33a0f500a9e0b254
darakolesnikova <dkolesnikova1999@bk.ru>
2022-11-13 18:09:25 +0300

    update  test

--- src/test/java/guru/qa/AutomationPractice.java
+++ src/test/java/guru/qa/AutomationPractice.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -31,6 +32,11 @@ public class AutomationPractice {
         $x("//div[@aria-label='Choose Sunday, January 8th, 2012']").click();
         $x("//div[contains(@class,'text-right col-md-2 col-sm-12')]").click();
 
+        $x("//*[contains(text(),'Student Name')]//following-sibling::td").shouldHave(text("Ilya Ivanov"));
+        $x("//*[contains(text(),'Student Email')]//following-sibling::td").shouldHave(text("mail@mail.ru"));
+        $x("//*[contains(text(),'Gender')]//following-sibling::td").shouldHave(text("Male"));
+        $x("//*[contains(text(),'Mobile')]//following-sibling::td").shouldHave(text("8800555353"));
+        $x("//*[contains(text(),'Date of Birth')]//following-sibling::td").shouldHave(text("08 January,2012"));
 
     }
 
