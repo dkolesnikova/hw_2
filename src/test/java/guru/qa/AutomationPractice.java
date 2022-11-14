70881c3c6b56e03202c2f20355aa40fd4fcff0ac
darakolesnikova <dkolesnikova1999@bk.ru>
2022-11-14 22:22:01 +0300

    update  test

--- src/test/java/guru/qa/AutomationPractice.java
+++ src/test/java/guru/qa/AutomationPractice.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
@@ -12,17 +14,27 @@ public class AutomationPractice {
 
     @BeforeAll
     static void configure() {
-        Configuration.browserSize = "700x400";
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
     void checkTheFormCanBeFilledInWithAllFields() {
+        //Arrange
+        String firstName = "Ilya";
+        String secondName = "Ivanov";
+        String mail = "mail@mail.ru";
+        String number = "8800555353";
+        String address = "Smolensk";
+        String subject = "English";
+        String state = "NCR";
+        String city = "Delhi";
+        //Act
         open("https://demoqa.com/automation-practice-form");
-        $x("//input[@id='firstName']").setValue("Ilya");
-        $x("//input[@id='lastName']").setValue("Ivanov");
-        $x("//input[@id='userEmail']").setValue("mail@mail.ru");
-        $x("//label[@for='gender-radio-1']").click();
-        $x("//input[@id='userNumber']").setValue("88005553535");
+        $x("//input[@id='firstName']").setValue(firstName);
+        $x("//input[@id='lastName']").setValue(secondName);
+        $x("//input[@id='userEmail']").setValue(mail);
+        $x("//label[contains(text(),'Male')]").click();
+        $x("//input[@id='userNumber']").setValue(number);
         $x("//input[@id='dateOfBirthInput']").click();
         $x("//select[contains(@class,'react-datepicker__year-select')]").click();
         $x("//option[@value='2012']").click();
@@ -30,14 +42,28 @@ public class AutomationPractice {
         $x("//select[contains(@class,'react-datepicker__month-select')]").click();
         $x("//option[@value='0']").click();
         $x("//div[@aria-label='Choose Sunday, January 8th, 2012']").click();
+        $x("//input[@id='subjectsInput']").setValue(subject);
+        $x("//input[@id='subjectsInput']").pressEnter();
+        $x("//label[contains(text(),'Sports')]").click();
+        $x("//label[contains(text(),'Reading')]").click();
+        $x("//label[contains(text(),'Music')]").click();
+        $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/images.jfif"));
+        $x("//textarea[@id='currentAddress']").setValue(address);
+        $x("//input[@id='react-select-3-input']").setValue(state);
+        $x("//input[@id='react-select-3-input']").pressEnter();
+        $x("//input[@id='react-select-4-input']").setValue(city);
+        $x("//input[@id='react-select-4-input']").pressEnter();
         $x("//div[contains(@class,'text-right col-md-2 col-sm-12')]").click();
-
-        $x("//*[contains(text(),'Student Name')]//following-sibling::td").shouldHave(text("Ilya Ivanov"));
-        $x("//*[contains(text(),'Student Email')]//following-sibling::td").shouldHave(text("mail@mail.ru"));
+        //Assert
+        $x("//*[contains(text(),'Student Name')]//following-sibling::td").shouldHave(text(firstName + " " + secondName));
+        $x("//*[contains(text(),'Student Email')]//following-sibling::td").shouldHave(text(mail));
         $x("//*[contains(text(),'Gender')]//following-sibling::td").shouldHave(text("Male"));
-        $x("//*[contains(text(),'Mobile')]//following-sibling::td").shouldHave(text("8800555353"));
+        $x("//*[contains(text(),'Mobile')]//following-sibling::td").shouldHave(text(number));
         $x("//*[contains(text(),'Date of Birth')]//following-sibling::td").shouldHave(text("08 January,2012"));
-
+        $x("//*[contains(text(),'Subjects')]//following-sibling::td").shouldHave(text(subject));
+        $x("//*[contains(text(),'Hobbies')]//following-sibling::td").shouldHave(text("Sports, Reading, Music"));
+        $x("//*[contains(text(),'Picture')]//following-sibling::td").shouldHave(text("images.jfif"));
+        $x("//*[contains(text(),'Address')]//following-sibling::td").shouldHave(text("Smolensk"));
+        $x("//*[contains(text(),'State and City')]//following-sibling::td").shouldHave(text(state + " " + city));
     }
-
 }
